edc6c9b437d2cd89bc4af03bdcfea123454ff0d5
olehae <das.typ.2003@gmail.com>
2022-12-10 21:14:32 +0100

    Day 10

--- Day 10/AOC 10.py
+++ Day 10/AOC 10.py
@@ -1,4 +1,4 @@
-with open("input10_fg.txt") as f:
+with open("input10.txt") as f:
     data = f.readlines()
 
 data = [i.replace("\n", "").split() for i in data]
@@ -19,4 +19,17 @@ print(f"{log=}\n")
 
 print(sum(log[i-1] * i for i in (20, 60, 100, 140, 180, 220)))
 
-print(len(log)/40)
+drawing = []
+
+for i in range(240):
+    if log[i]+1 >= i % 40 >= log[i]-1:
+        drawing.append("#")
+    else:
+        drawing.append(" ")
+
+print(f"\n{drawing=}\n")
+
+for i in range(len(drawing)):
+    print(drawing[i], end="")
+    if i in (39, 79, 119, 159, 199):
+        print()
